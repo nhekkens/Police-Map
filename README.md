3e3da8588487994d2f7a0b81f068030d4c7fb6c6
“Nicolay Hekkens <mailto:nicolay.hekkens@purestone.co.uk>
2017-12-06 16:26:11 +0000

    Setup webpack build, update readme, add Sass Compiler

---
+++ README.md
@@ -0,0 +1,23 @@
+# Police Map
+
+> A Vue.js project using the UK police API
+
+## Build Setup
+
+``` bash
+# install dependencies
+node
+npm
+npm install
+
+# serve with hot reload at localhost:1337
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation about the [task](https://github.com/syzygy-uk/crime-map)
---
+++ src/components/Homepage.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="containerFull">
+		<router-view @route-data-loaded="changeTitle"></router-view>
+
+		
+	</div>
+</template>
+
+<script>
+	import { EventBus } from './Parts/EventBus';
+
+	export default {
+		components: {},
+		name: 'Homepage',
+		mounted() {
+			this.changeTitle(this.dataName);
+		},
+		props: {
+			dataUrl: {
+				type: String,
+				default: 'unassigned'
+		    }
+		},
+		data () {
+			return {
+				logging: true,
+				dataName: 'Leaky - Home'
+			}
+		},
+		methods: {
+			changeTitle(Name) {
+				if (this.logging) { console.log('Leaky •• changeTitle: ' + Name); }
+				document.title = Name
+			}	
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./../assets/scss/base.scss" lang="scss">
+
+</style>
